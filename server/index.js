d4849de852e33769336bff84526725885d0d09c3
DOM_BCI_PROD\EXIDIGO <idigonza@everis.com>
2018-04-05 15:20:57 -0300

    correccion queries

--- server/index.js
+++ server/index.js
@@ -26,93 +26,55 @@ app.use(function (req, res, next) {
 
 app.get('/groups', (req, resp) => {
 
-  let grupo = {
-    grupo: {},
-    partidos: []
-  }
-
-  let respuesta = {
-    grupos: [grupo]
-  }
-
   connection.getConnection((error, tempCont) => {
+    var promises = [];
     if (!!error) {
       tempCont.release();
       console.log('Error');
     } else {
-      tempCont.query(`SELECT grupo FROM equipoPais WHERE competicion_equipoPais = 1 GROUP BY grupo
-      `, (error, results, fields) => {
+      tempCont.query(`SELECT grupo FROM equipoPais WHERE competicion_equipoPais = 1 GROUP BY grupo`
+      , (error, groups, fields) => {
           // tempCont.release();
           if (!!error) {
             console.log('Error in query');
           } else {
             // resp.json(results);
-            for (i = 0; i < results.length; i++) {
-              // grupo = null;
-              grupo.grupo = results[i].grupo;
-              console.log(results[i].grupo);
-              const PARTIDOS = getPartidos(tempCont, results[i].grupo);
-
-              PARTIDOS.then((result) => {
-                grupo.partidos = results;
-                respuesta.grupos.push(JSON.stringify(grupo));
-                console.log('agrega');
-              });
+            for (const group of groups) {
+              promises.push(getPartidos(tempCont, group.grupo));
             }
             tempCont.release();
-            resp.json(respuesta);
-            console.log('respuesta', respuesta);
+            Promise.all(promises).then((res) => {
+              // console.log('res', res);
+              resp.json(res);
+            });
           }
         });
     }
   });
 });
 
-function getPartidos(tempCont, group) {
-  return new Promise((resolve, reject) => {
+async function getPartidos(tempCont, group) {
+  return await new Promise((resolve, reject) => {
     tempCont.query(`SELECT partidos.idPartido, partidos.fechaPartido, EA.nombre as NombreA, EB.nombre as NombreB, EA.keyname as keynameA, EB.keyname as keynameB
     FROM partidos
     INNER JOIN equipopais EA ON partidos.equipoPaisA = EA.idEquipoPais
     INNER JOIN equipopais EB ON partidos.equipoPaisB = EB.idEquipoPais
-    WHERE partidos.equipoPaisA IN (SELECT idEquipoPais FROM equipopais WHERE equipopais.grupo = 'A' )
-    OR partidos.equipoPaisB IN (SELECT idEquipoPais FROM equipopais WHERE equipopais.grupo = 'A' )
+    WHERE partidos.equipoPaisA IN (SELECT idEquipoPais FROM equipopais WHERE equipopais.grupo = '${group}' )
+    OR partidos.equipoPaisB IN (SELECT idEquipoPais FROM equipopais WHERE equipopais.grupo = '${group}' )
     ORDER BY partidos.idPartido`, (error, results, fields) => {
         // tempCont.release();
         if (!!error) {
           console.log('Error in query');
         } else {
-          resolve(results);
-          // grupo.partidos = results;
-          // console.log(grupo.grupo);
-          // respuesta.grupos.push(JSON.stringify(grupo));
-          // console.log(respuesta);
+          let grupo = null;
+          grupo = {
+            grupo: group,
+            partidos: results
+          }
+          resolve(grupo);
         }
       });
   });
 }
-
-app.get('/partidos', (req, resp) => {
-  connection.getConnection((error, tempCont) => {
-    if (!!error) {
-      tempCont.release();
-      console.log('Error');
-    } else {
-      tempCont.query(`SELECT partidos.idPartido, partidos.fechaPartido, EA.nombre as NombreA, EB.nombre as NombreB, EA.keyname as keynameA, EB.keyname as keynameB
-      FROM partidos
-      INNER JOIN equipopais EA ON partidos.equipoPaisA = EA.idEquipoPais
-      INNER JOIN equipopais EB ON partidos.equipoPaisB = EB.idEquipoPais
-      WHERE partidos.equipoPaisA IN (SELECT idEquipoPais FROM equipopais WHERE equipopais.grupo = 'A' )
-      OR partidos.equipoPaisB IN (SELECT idEquipoPais FROM equipopais WHERE equipopais.grupo = 'A' )
-      ORDER BY partidos.idPartido`, (error, results, fields) => {
-          tempCont.release();
-          if (!!error) {
-            console.log('Error in query');
-          } else {
-            resp.json(results);
-          }
-        });
-    }
-  });
-});
 
 app.listen('1337');
