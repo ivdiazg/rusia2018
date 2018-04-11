24b932fa55f98b564a969589ae9f7541951a3e39
DOM_BCI_PROD\EXIDIGO <idigonza@everis.com>
2018-04-11 19:12:28 -0300

    corrige metodo para actualizar apuestas

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var app = express();
 
@@ -9,6 +10,18 @@ var connection = mysql.createPool({
   password: '',
   database: 'rusia2018'
 });
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  // IE9 doesn't set headers for cross-domain ajax requests
+  if (typeof (req.headers['content-type']) === 'undefined') {
+    req.headers['content-type'] = "application/json; charset=UTF-8";
+  }
+  next();
+})
+  .use(bodyParser.json());
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
@@ -41,9 +54,8 @@ app.get('/groups', (req, resp) => {
             for (const group of groups) {
               promises.push(getPartidos(tempCont, group.grupo));
             }
-            tempCont.release();
             Promise.all(promises).then((res) => {
-              // console.log('res', JSON.stringify(res));
+              tempCont.release();
               resp.json(res);
             });
           }
@@ -54,16 +66,6 @@ app.get('/groups', (req, resp) => {
 
 async function getPartidos(tempCont, group) {
   return await new Promise((resolve, reject) => {
-    // tempCont.query(`SELECT partidos.idPartido, partidos.fechaPartido, EA.nombre as NombreA
-    //   , apuestas.golesA, apuestas.golesB, EB.nombre as NombreB, EB.nombre as NombreB
-    //   , EA.keyname as keynameA, EB.keyname as keynameB, partidos.habilitado
-    //   FROM partidos
-    //   INNER JOIN equipopais EA ON partidos.equipoPaisA = EA.idEquipoPais
-    //   INNER JOIN equipopais EB ON partidos.equipoPaisB = EB.idEquipoPais
-    //   LEFT JOIN apuestas ON partidos.idPartido = apuestas.partido_apuesta AND apuestas.participante = 1
-    //   WHERE partidos.equipoPaisA IN (SELECT idEquipoPais FROM equipopais WHERE equipopais.grupo = '${group}' )
-    //   OR partidos.equipoPaisB IN (SELECT idEquipoPais FROM equipopais WHERE equipopais.grupo = '${group}' )
-    //   ORDER BY partidos.idPartido`, (error, results, fields) => {
     tempCont.query(`CALL groupsEquipos('${group}', 1)`, (error, results, fields) => {
       // tempCont.release();
       if (!!error) {
@@ -82,47 +84,78 @@ async function getPartidos(tempCont, group) {
 
 app.get('/matchesOfTheDay', (req, resp) => {
   connection.getConnection((error, tempCont) => {
-    var promises = [];
     if (!!error) {
       tempCont.release();
       console.log('Error');
     } else {
       participante = 1;
       tempCont.query(`CALL matchesOfTheDay (${participante})`, (error, results, fields) => {
-          // tempCont.release();
-          if (!!error) {
-            console.log('Error in query');
+        // tempCont.release();
+        if (!!error) {
+          console.log('Error in query');
+        } else {
+          tempCont.release();
+          let tiles = [];
+          let cols = 0;
+          let colspanVal = 0;
+          const COLORS = ['lightblue', 'lightgreen', 'lightpink', 'lightyellow'
+            , 'lightblue', 'lightgreen', 'lightpink', 'lightyellow'
+            , 'lightblue', 'lightgreen', 'lightpink', 'lightyellow'];
+
+          if (results[0].length === 3 || results[0].length > 4) {
+            cols = 6;
+            colspanVal = 2;
           } else {
-            tempCont.release();
-            let tiles = [];
-            let cols = 0;
-            let colspanVal = 0;
-            const COLORS = ['lightblue', 'lightgreen', 'lightpink', 'lightyellow'
-              , 'lightblue', 'lightgreen', 'lightpink', 'lightyellow'
-              , 'lightblue', 'lightgreen', 'lightpink', 'lightyellow'];
-
-            if (results[0].length === 3 || results[0].length > 4) {
-              cols = 6;
-              colspanVal = 2;
-            } else {
-              cols = 6;
-              colspanVal = 3;
-            }
-
-            for (i = 0; i < results[0].length; i++) {
-              tiles.push({ colspan: colspanVal, rowspan: colspanVal, color: COLORS[i] });
-            }
-
-            send = {
-              'cols': cols,
-              'tiles': tiles,
-              'matches': results[0]
-            }
-            resp.json(send);
+            cols = 6;
+            colspanVal = 3;
           }
-        });
+
+          for (i = 0; i < results[0].length; i++) {
+            tiles.push({ colspan: colspanVal, rowspan: colspanVal, color: COLORS[i] });
+          }
+          send = {
+            'cols': cols,
+            'tiles': tiles,
+            'matches': results[0]
+          }
+          resp.json(send);
+        }
+      });
     }
   });
 });
+
+app.post('/updApuestaMatch', (req, resp) => {
+  var promises = [];
+  connection.getConnection((error, tempCont) => {
+    if (!!error) {
+      tempCont.release();
+      console.log('Error');
+    } else {
+      req.body.forEach(match => {
+        promises.push(updApuesta(tempCont, match));
+      });
+      Promise.all(promises).then((res) => {
+        tempCont.release();
+        resp.json(res);
+      }).catch(err => {
+        console.log(err);
+      });
+    }
+  });
+});
+
+async function updApuesta(tempCont, match) {
+  return await new Promise((resolve, reject) => {
+    tempCont.query(`CALL updApuestaMatch (${match.idPartido}, ${match.golesA}, ${match.golesB}, ${match.competicion}, ${match.participante})`
+      , (error, groups, fields) => {
+        if (!!error) {
+          console.log('Error in query');
+        } else {
+          resolve(true);
+        }
+      });
+  });
+}
 
 app.listen('1337');
